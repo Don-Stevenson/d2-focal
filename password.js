e5f03ab90124a6151f07cd6814557d3748528462
Don-Stevenson <dsteven1@gmail.com>
2019-11-26 18:41:09 +0000

    working version of password obfuscate.js

---
+++ password.js
@@ -0,0 +1,25 @@
+let args = process.argv.slice(2);
+
+
+oldPass = args.join();
+
+
+const obfuscate = (input) => {
+  let newPass = '';
+  for (let i = 0; i < oldPass.length; i++) {
+  if (oldPass[i] === "a"){
+      newPass += "4";
+    } else if (oldPass[i] === "e") { 
+      newPass += "3";
+    } else if (oldPass[i] === "i") { 
+      newPass += "1";
+    } else if (oldPass[i] === "o") { 
+      newPass += "0";
+    } else { 
+      newPass += oldPass[i];
+    }
+  }
+ return newPass;
+};
+
+console.log(obfuscate(args));
